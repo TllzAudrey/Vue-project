1ca43065f5d56f5cd68ebdf83d48fd2e4103b8c0
Audrey Telliez <146714723+TllzAudrey@users.noreply.github.com>
2025-09-24 11:36:58 +0200

    update starter project

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+  import { ref } from 'vue'
+  const name = ref('')
+  const project = ref('')
+  const text = ref('')
+  const reponse = ref(true)
+
+  function toggle() {
+    reponse.value = !reponse.value
+  }
+</script>
+
+<template>
+  <h1>Formulaire </h1>
+    <input v-model="name" placeholder="Type your name">
+    <input v-model="project" placeholder="Type the name of your project">
+    <input v-model="text" placeholder="Type text">
+  <button @click="toggle">Envoie formulaire</button>
+  <p v-if="reponse"></p>
+  <p v-else>{{ name }}, {{ project }}, {{ text }}</p>
+</template>
+
+<style scoped></style>
